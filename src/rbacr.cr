29c6164117b3b40574588f602241b6f4a33b6b83
Gilberdi Sinaga <gilberdising012@gmail.com>
2025-09-14 22:35:40 +0700

    Initialize project structure

---
+++ src/rbacr.cr
@@ -0,0 +1,5 @@
+require "./rbacr/exceptions"
+
+module Rbacr
+  VERSION = "0.1.0"
+end
---
+++ src/rbacr/exceptions.cr
@@ -0,0 +1,20 @@
+class Rbacr::Error < Exception
+end
+
+class Rbacr::UnknownRoleError < Rbacr::Error
+  def initialize(role_name : String | Symbol)
+    super("Unknown role: #{role_name}")
+  end
+end
+
+class Rbacr::UnknownPrivilegeError < Rbacr::Error
+  def initialize(privilege_id : String | Symbol)
+    super("Unknown privilege: #{privilege_id}")
+  end
+end
+
+class Rbacr::AuthorizationError < Rbacr::Error
+  def initialize(message : String = "Access denied")
+    super(message)
+  end
+end
